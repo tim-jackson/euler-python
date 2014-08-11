f28f02f30ac4691f002acf123be20dd15d621a0b
Tim Jackson <tim.jackson@truphone.com>
2014-08-11 08:31:44 +0100

    Adding solution to problem 10.

---
+++ Problem10/primesum.py
@@ -0,0 +1,33 @@
+"""primesum.py: Problem 10 of project Euler.
+Calculates the sum of prime numbers below 2000000."""
+
+# NOTE: This is quite slow. Need to reduce the pool of
+# potential prime numbers dramatically.
+
+import math
+
+TOTAL = 2
+I = 3
+MAX_NUMBER = 2000000
+
+def is_prime_number(number):
+    """ Checks if a number is prime. """
+
+    sqtnum = math.sqrt(number)
+
+    j = 2
+    while j <= sqtnum:
+        if number % j == 0:
+            return False
+        j += 1
+
+    return True
+
+if __name__ == "__main__":
+    while I < MAX_NUMBER:
+        if is_prime_number(I):
+            TOTAL = TOTAL + I
+        I += 1
+
+    print "The sum of prime numbers under {0} is {1}".format(MAX_NUMBER, TOTAL)
+
